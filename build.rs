8654db05872581b2bcc49d04e50de22c1a6aa123
FeeeeK <sign@axd1x8a.me>
2025-07-03 20:52:38 +0300

    Implement `std::list`

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     cc::Build::new()
-        .files(["CSTL/lib/type.c", "CSTL/lib/vector.c", "CSTL/lib/xstring.c"])
+        .files(["CSTL/lib/type.c", "CSTL/lib/vector.c", "CSTL/lib/xstring.c", "CSTL/lib/list.c"])
         .std("c11")
         .compile("CSTL");
 }
