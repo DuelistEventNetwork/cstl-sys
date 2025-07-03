77c2cfcbf9b1a6eadf1b0f81a1da3c693c591c7d
FeeeeK <sign@axd1x8a.me>
2025-07-03 20:56:41 +0300

    Update build script

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 fn main() {
+    // rerun if any of the source files change
+    println!("cargo:rerun-if-changed=CSTL/lib");
     cc::Build::new()
-        .files(["CSTL/lib/type.c", "CSTL/lib/vector.c", "CSTL/lib/xstring.c", "CSTL/lib/list.c"])
+        .files(["CSTL/lib/type.c", "CSTL/lib/vector.c", "CSTL/lib/xstring.c", "CSTL/lib/list.c", "CSTL/lib/alloc.c"])
         .std("c11")
         .compile("CSTL");
 }
